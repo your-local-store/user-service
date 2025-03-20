15881e2d400d959a5e296e00b564d0576e5e3621
ALGORITHM-VIBES <mcadk.s@gmail.com>
2025-03-21 01:10:00 +0530

    project initialized

---
+++ src/main/java/dev/mcadks/ecommerceuserservice/EcommerceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.mcadks.ecommerceuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mcadks/ecommerceuserservice/EcommerceUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mcadks.ecommerceuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
